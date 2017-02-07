0cffe14a5129ef3ad8fb279f9c807908f9f45454
jakesmorrison <jakesmorrison4@gmail.com>
2017-02-07 11:42:38 -0700

    apache compat

--- country_floral/apache/override.py
+++ country_floral/apache/override.py
@@ -3,7 +3,7 @@
 from country_floral.settings import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['www.countryfloralidaho.com','countryfloralidaho.com', '192.241.213.57']
+ALLOWED_HOSTS = ['www.countryfloralidaho.com','countryfloralidaho.com', '198.199.102.149']
 
 DATABASES = {
     'default': {
