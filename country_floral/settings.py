e8bf78e8683dbc7a55e820812f1d755145dda756
jakesmorrison <jakesmorrison4@gmail.com>
2018-09-17 15:19:45 -0600

    debug false

--- country_floral/settings.py
+++ country_floral/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n73i6d*+7*yq7i717kz-3y_waq-ht(c_da%d!u7vxwi5%*%0mb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
