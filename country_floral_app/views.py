899ea00389f16ffa04b941e0213743fc8560fd96
JAKOB MORRISON <JMORRISON@MICRON.COM>
2020-04-17 13:05:59 -0600

    changing button

--- country_floral_app/views.py
+++ country_floral_app/views.py
@@ -305,11 +305,11 @@ def process(request):
 
 
 def confirm(request):
-    if cfg.CONFIG.ORDERNUMBER != -1:
+    params = request.GET
+    # order_number = int(max(list(Floral.objects.values_list("order_number", flat=True))))
+    order_number = params["order_number"]
+    if cfg.CONFIG.ORDERNUMBER == order_number:
         try:
-            params = request.GET
-            # order_number = int(max(list(Floral.objects.values_list("order_number", flat=True))))
-            order_number = params["order_number"]
             context = Floral.objects.all().filter(order_number=order_number).values()[0]
         except Exception as e:
             write_to_error_file("#"+str(order_number) + " failed:"  + str(e))
